d0bfb417c91c9a89660115952fca6a7ad9e58309
shiva <shiva@gmail.com>
2024-03-08 16:25:27 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>Multimart</h1>
+              </div>
+              <p>Get exclusive branded products at low cost</p>
+              <p>Fashion | Explore</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>Near Talent Sprint, Gachibowli, Telangana-500081 </li>
+                <li>Email: multimart.help@gmail.com</li>
+                <li>Phone: +91 4000512362</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
